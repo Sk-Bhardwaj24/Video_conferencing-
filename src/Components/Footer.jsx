0744b1e5e29f6ae0f89b4fdb8c53a5a583bd5a28
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-06-13 22:52:24 +0530

    added footer components

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useAVToggle } from "@100mslive/react-sdk";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+function Footer() {
+  const { isLocalAudioEnabled, isLocalVideoEnabled, toggleAudio, toggleVideo } =
+    useAVToggle();
+  return (
+    <div className="control-bar">
+      <button className="btn-control" onClick={toggleAudio}>
+        {isLocalAudioEnabled ? "Mute" : "Unmute"}
+      </button>
+      <button className="btn-control" onClick={toggleVideo}>
+        {isLocalVideoEnabled ? "Hide" : "Unhide"}
+      </button>
+    </div>
+  );
+}
 
 export default Footer;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import {
+  selectIsConnectedToRoom,
+  useHMSActions,
+  useHMSStore,
+} from "@100mslive/react-sdk";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+function Navbar() {
+  const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const hmsActions = useHMSActions();
+
+  return (
+    <nav>
+      <img
+        className="logo"
+        src="https://ashwins93.app.100ms.live/static/media/100ms_logo.3cfd8818.svg"
+        alt="logo"
+      />
+      {isConnected && (
+        <button
+          id="leave-btn"
+          className="btn-danger"
+          onClick={() => hmsActions.leave()}
+        >
+          Leave Room
+        </button>
+      )}
+    </nav>
+  );
+}
 
 export default Navbar;
